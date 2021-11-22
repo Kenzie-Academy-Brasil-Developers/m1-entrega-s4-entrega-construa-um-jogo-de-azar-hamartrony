53f6f7449dee526e433410a3e6822278ad63e246
Hamart Rony <hamart23@icloud.com>
2021-11-22 12:08:26 -0300

    array criado

---
+++ script.js
@@ -0,0 +1,3 @@
+const palavras = ['abelha','banana','cadeira','decolar','esmola','faca',
+ 'galo', 'hiper', 'imortal','janela','kenzie','lago', 'madeira', 'nave',
+ 'ovo','porco','queijo','rabanete','sabonete','topeira']
