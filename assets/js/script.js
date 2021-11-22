03ce0aab8edc93511e46891deff43d9e0523343d
David Almeida <contato.davidalmeida@outlook.com>
2021-11-22 15:03:43 -0300

    Criando esqueleto para o HTML pelo style.css, incluindo reset.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 const palavras = ['abelha','banana','cadeira','decolar','esmola','faca',
  'galo', 'hiper', 'imortal','janela','kenzie','lago', 'madeira', 'nave',
- 'ovo','porco','queijo','rabanete','sabonete','topeira']
+ 'ovo','porco','queijo','rabanete','sabonete','topeira'];
+
+console.log(palavras)
