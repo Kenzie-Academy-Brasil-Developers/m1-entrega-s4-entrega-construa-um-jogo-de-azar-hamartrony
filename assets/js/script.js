19480a79d6caa488bf2d71f13e88192c143043ff
kennedybm <kennedy_muniz@hotmail.com>
2021-11-23 13:00:44 -0300

    adicionando cursor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,55 @@
 const palavras = ['abelha','banana','cadeira','decolar','esmola','faca',
  'galo', 'hiper', 'imortal','janela','kenzie','lago', 'madeira', 'nave',
- 'ovo','porco','queijo','rabanete','sabonete','topeira'];
+ 'ovo','porco','queijo','rabanete','sabonete','topeira']
+ 
+ function geradorDeLetra() {
 
-console.log(palavras)
+    const alfabeto = "abcdefghijklmnopqrstuvwxyz"
+  
+    return alfabeto[Math.floor(Math.random() * alfabeto.length)]
+ }
+ geradorDeLetra()
+
+ let currentPlayer = 'X';   //jogador da vez variavel
+let nextPlayer = 'O';       //proximo jogador variavel
+
+let playerXSelections = [];     //array que armazena os clicks
+let playerOselections = [];
+
+const winningCombinations = [   //array com as combincoes de vitoria - 
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+]
+
+
+
+const handleClik = function(event){             //click na celula
+    const cell = event.target
+    console.log(cell.id);
+}
+const cells = document.querySelectorAll('td');
+console.log(cells)
+
+for (let i=0; i<cells.length; i++ ){
+    cells[i].addEventListener('click', handleClik);
+    cells[i].innerText = geradorDeLetra()
+}
+
+
+function geradorDeLetra() {
+
+    const alfabeto = "abcdefghijklmnopqrstuvwxyz"
+  
+    return alfabeto[Math.floor(Math.random() * alfabeto.length)]
+ }
+ geradorDeLetra()
+ 
+
+
+
