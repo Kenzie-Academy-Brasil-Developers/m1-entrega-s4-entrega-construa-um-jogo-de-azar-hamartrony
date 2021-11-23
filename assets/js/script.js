5b41e201f94ce5d097d718e972e7d62219e51730
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2021-11-22 20:45:10 -0500

    beta

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,39 @@ const palavras = ['abelha','banana','cadeira','decolar','esmola','faca',
  'ovo','porco','queijo','rabanete','sabonete','topeira'];
 
 console.log(palavras)
+
+function catchs(){
+
+    const a = document.getElementById("a")
+    const m = document.getElementById("m")
+    const o = document.getElementById("o")
+    const r = document.getElementById("r")
+
+    a.addEventListener('click', tester)
+    m.addEventListener('click', tester)
+    o.addEventListener('click', tester)
+    r.addEventListener('click', tester)
+
+}
+
+let arrcreated = [];
+
+function tester(){
+
+    const arright = "amor";
+    let strcreated;
+    const events = event.target.id;
+    arrcreated.push(events);
+
+    if (arrcreated.length == arright.length){
+        strcreated = arrcreated.join("");
+    }else if (arrcreated.length > arright.length){
+        arrcreated = [];
+
+    }
+    if ( strcreated  == arright){
+        alert("correto!")
+    }
+}
+
+catchs()
