6ca2326ed1a8da3b2177f8665cafb4290efce7ba
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2021-11-23 19:00:34 -0500

    main

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const palavras = ['abelha','banana','cadeira','decolar','esmola','faca',
- 'galo', 'hiper', 'imortal','janela','kenzie','lago', 'madeira', 'nave',
- 'ovo','porco','queijo','rabanete','sabonete','topeira'];
-
-console.log(palavras)
