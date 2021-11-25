5013e43430cf1112669653b1eeb15ca2c0a3cf03
bryantorresribeiro <bryant0rr3sribeiro@gmail.com>
2021-11-24 22:49:37 -0500

    metade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //ARRAY DE PALAVRAS DO JOGO
 const palavras = ['comensal','hermione','sonserina','azkaban','horcrux','nimbus',
 'dobby', 'hagrid', 'potter','mafoy','ronald','draco', 'severus', 'sirius',
-'cornival','harry','hermione','minerva','neville','edwiges']
+'cornival','harry','minerva','neville','edwiges']
 //ADICIONADO TODAS AS ALTERAÇÕES A TABLEA
 const cells = document.querySelectorAll('td');
 //GERADOR DE LETRAS ALEATORIAS NA TABELA
@@ -10,12 +10,12 @@ function geradorDeLetra(){
     return alfabeto[Math.floor(Math.random() * alfabeto.length)]
 }
 //FUNÇÃO QUE ESCOLHE 1 PALAVRA ALEATORIA DENTRO DO ARRAY DE PALAVRAS 
-let result = [];
-    function geradorDePalavra(){
+function geradorDePalavra(){
+    let result = [];
     for(let i = 0; i < 1; i++){
     result.push(palavras[aleatorio0a19()])
     }
-    return result.join(" ")
+    return result.join("")
 }
 //FUNÇÃO QUE ADICIONA AS LETRAS ALEATORIAS NA TABELA
 function adicionaLetras(){
@@ -24,28 +24,94 @@ function adicionaLetras(){
     }
 }
 adicionaLetras()
-//FUNÇÃO QUE DEVOLVE UM NUMERO ALEATORIO DE 1 A 10
+//FUNÇÃO QUE DEVOLVE UM NUMERO ALEATORIO DE 0 A 10
 function aleatorio1a10() {
-    return Math.floor(Math.random()* 10 + 1);   
+let ramdom =  Math.floor(Math.random(1, 10)* 10 + 1);   
+    return ramdom;
 }
 //FUNÇÃO QUE DEVOLVE UM NUMERO ALEATORIO DE 0 19
 function aleatorio0a19() {
     return Math.floor(Math.random()* 19 + 1);   
 }
-//FUNÇÃO QUE JOGA A PRIMEIRA PALAVRAS DE FORMA VERTICAL 
+//ESCOLHE UMA COLUNA DENTRO DO ARRAY DE COLUNAS
+const coluna = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91];
+const colunaramdom = coluna[aleatorio1a10()];
+//FUNCAO QUE JOGA A PRIMEIRA PALAVRAS DE FORMA VERTICAL 
 const palavra = geradorDePalavra();
+let arrayLetraClicada = []
+let arrayPalavraAleatoriaInserida = [];
+function vertical(){
     let j = 0;
-    let arrayPalavraAleatoriaInserida = []
-    let arrayLetraClicada = []
     for (let i = aleatorio1a10(); i<cells.length; i+=10){        
-    if ( palavra[j] === undefined){
-        cells[i].innerText = geradorDeLetra()
-    }else{
-        cells[i].innerText = palavra[j];
-        j+=1;
-        arrayPalavraAleatoriaInserida.push(cells[i].id)    
+        if ( palavra[j] === undefined ){
+           break;
+        }else{
+            cells[i].innerText = palavra[j];
+            console.log(palavra[j]);
+            arrayPalavraAleatoriaInserida.push(cells[i].id)    
+            j+=1;
+        }
     }
 }
+//FUNCAO QUE JOGA A SEGUNDA PALAVRA DE FORMA HORIZONTAL
+let palavra2 = geradorDePalavra();
+let arrayPalavraAleatoriaInserida2 = []
+function horizontal1(){
+    console.log("teste");
+    j = 0;
+    for (let i = colunaramdom; i<cells.length; i++){        
+        if ( palavra2[j] === undefined){
+            break;
+        }else{
+            cells[i].innerText = palavra2[j];
+            console.log(palavra2[j]);
+            arrayPalavraAleatoriaInserida2.push(cells[i].id)    
+            j+=1;
+        }
+    }
+}
+//FUNCAO QUE JOGA A TERCEIRA PALAVRA DE FORMA HORIZONTAL
+//let palavra3 = geradorDePalavra();
+//let arrayPalavraAleatoriaInserida3 = []
+//function horizontal2(){
+//    j = 0;
+//    for (let i = colunaramdom; i<cells.length; i++){        
+//        if ( palavra3[j] === undefined){
+//            break;
+//        }else{
+//            cells[i].innerText = palavra3[j];
+//            console.log(palavra3[j]);
+//            arrayPalavraAleatoriaInserida3.push(cells[i].id)    
+//            j+=1;
+//        }
+//    }
+//}
+vertical()
+horizontal1()
+//horizontal2()
+//FUNÇÃO QUE VERIFICA A COLISÃO DE PALAVRAS
+    let maior; 
+if (palavra.length > palavra2.length){
+    maior = palavra;
+}else{
+    maior = palavra2;
+}
+
+console.log(palavra)
+console.log(palavra2)
+console.log()
+
+function colisao(){
+  for (let i=0; i<maior.length; i++){
+     for (let j = 0; j<maior.length; j++){
+     if (arrayPalavraAleatoriaInserida[i] === arrayPalavraAleatoriaInserida2[j] 
+        || palavra == palavra2 || palavra == "" || palavra2 == ""){
+      
+    location.reload();
+}
+}}
+}
+colisao()
 //FUNÇÃO QUE CAPTURA O ID DAS LETRAS
 function ClickTabela(){
     const cell = event.target.id;
@@ -56,24 +122,59 @@ function validacaoDeDados(){
     if (arrayPalavraAleatoriaInserida.length == arrayLetraClicada.length){
         let string1 = arrayLetraClicada.join("");
         let string2 = arrayPalavraAleatoriaInserida.join("");
+        
         if (string1 == string2){
             alert("Acertou a frase!");
+            arrayLetraClicada = [];
         }else{
             alert("Errou a frase!");
             arrayLetraClicada = [];
         }
     }
+    if (arrayPalavraAleatoriaInserida2.length == arrayLetraClicada.length){
+        let string1 = arrayLetraClicada.join("");
+        let string3 = arrayPalavraAleatoriaInserida2.join("");
+        console.log(string1)
+        console.log(string3)
+        
+        if (string1 == string3){
+            alert("Acertou a frase!");
+            arrayLetraClicada = [];
+        }else{
+            alert("Errou a frase!");
+            arrayLetraClicada = [];
+        }
+    }
+    
 }
 //EVENTOS DE CLICK SENDO ESPERADOS
 for (let i=0; i<cells.length; i++ ){
     cells[i].addEventListener('click', ClickTabela);
     cells[i].addEventListener('click', validacaoDeDados);  
+    cells[i].addEventListener('click', color);  
+    cells[i].addEventListener('click', function(){
+        if(clicks){
+            click();
+        }
+       
+});
+function color(){
+    cells[i].classList = "blue";
+}
 }
 //ADICIONANDO MUSICA A PAGINA
+const clicks = document.getElementById('click');
 const player = document.getElementById('audio');
+
 const button = document.getElementById('musica');
 function play() {
-    player.play()
+    player.play();
+   
+   
+    
+}
+function click(){
+    clicks.play();
 }
  function pause() {
     player.pause();
@@ -83,3 +184,7 @@ function play() {
              play();
          }else{pause();}
 });
+
+
+  
+
